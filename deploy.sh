90aecd184b9bc44b25f257de14e21c878d9b77ab
flipUp02 <80167896+flipUp02@users.noreply.github.com>
2024-11-05 16:49:12 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,179 +1,135 @@
-#!/bin/bash
+ARG postgresql_major=16
 
-# Env Vars
-POSTGRES_USER="myuser"
-POSTGRES_PASSWORD=$(openssl rand -base64 12)  # Generate a random 12-character password
-POSTGRES_DB="mydatabase"
-SECRET_KEY="my-secret" # for the demo app
-NEXT_PUBLIC_SAFE_KEY="safe-key" # for the demo app
-DOMAIN_NAME="nextselfhost.dev" # replace with your own
-EMAIL="your-email@example.com" # replace with your own
+FROM postgres:${postgresql_major} as base
 
-# Script Vars
-REPO_URL="https://github.com/flipUp02/testnext.git"
-APP_DIR=~/myapp
-SWAP_SIZE="1G"  # Swap size of 1GB
+FROM base as pgx_ulid
 
-# Update package list and upgrade existing packages
-sudo apt update && sudo apt upgrade -y
+# # postgres 17
+# ADD "https://github.com/HRKings/pgx_ulid/releases/download/v0.2.0/pgx_ulid-v0.2.0-pg17-amd64-linux-gnu.deb" \
+#     /tmp/pgx_ulid.deb
 
-# Add Swap Space
-echo "Adding swap space..."
-sudo fallocate -l $SWAP_SIZE /swapfile
-sudo chmod 600 /swapfile
-sudo mkswap /swapfile
-sudo swapon /swapfile
+ADD "https://github.com/pksunkara/pgx_ulid/releases/download/v0.1.5/pgx_ulid-v0.1.5-pg14-amd64-linux-gnu.deb" \
+    /tmp/pgx_ulid.deb
 
-# Make swap permanent
-echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+####################
+# Collect extension packages
+####################
+FROM scratch as extensions
+COPY --from=pgx_ulid /tmp/*.deb /tmp/
 
-# Install Docker
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
-sudo apt update
-sudo apt install docker-ce -y
+####################
+# Build final image
+####################
+FROM base as production
 
-# Install Docker Compose
-sudo rm -f /usr/local/bin/docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/v2.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# Setup extensions
+COPY --from=extensions /tmp /tmp
 
-# Wait for the file to be fully downloaded before proceeding
-if [ ! -f /usr/local/bin/docker-compose ]; then
-  echo "Docker Compose download failed. Exiting."
-  exit 1
-fi
+RUN apt-get update && apt-get install -y --no-install-recommends \
+    /tmp/*.deb \
+    && rm -rf /var/lib/apt/lists/* /tmp/*
 
-sudo chmod +x /usr/local/bin/docker-compose
 
-# Ensure Docker Compose is executable and in path
-sudo ln -sf /usr/local/bin/docker-compose /usr/bin/docker-compose
+# sudo docker ps -a
 
-# Verify Docker Compose installation
-docker-compose --version
-if [ $? -ne 0 ]; then
-  echo "Docker Compose installation failed. Exiting."
-  exit 1
-fi
+# sudo docker stop ca92d4ff3043
 
-# Ensure Docker starts on boot and start Docker service
-sudo systemctl enable docker
-sudo systemctl start docker
+# sudo docker rm ca92d4ff3043
 
-# Clone the Git repository
-if [ -d "$APP_DIR" ]; then
-  echo "Directory $APP_DIR already exists. Pulling latest changes..."
-  cd $APP_DIR && git pull
-else
-  echo "Cloning repository from $REPO_URL..."
-  git clone $REPO_URL $APP_DIR
-  cd $APP_DIR
-fi
+# sudo docker logs --since=1h ca92d4ff3043
 
-# For Docker internal communication ("db" is the name of Postgres container)
-DATABASE_URL="postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@db:5432/$POSTGRES_DB"
+# sudo docker rm -f $(sudo docker ps -a -q)
 
-# For external tools (like Drizzle Studio)
-DATABASE_URL_EXTERNAL="postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@localhost:5432/$POSTGRES_DB"
+# sudo docker volume ls
 
-# Create the .env file inside the app directory (~/myapp/.env)
-echo "POSTGRES_USER=$POSTGRES_USER" > "$APP_DIR/.env"
-echo "POSTGRES_PASSWORD=$POSTGRES_PASSWORD" >> "$APP_DIR/.env"
-echo "POSTGRES_DB=$POSTGRES_DB" >> "$APP_DIR/.env"
-echo "DATABASE_URL=$DATABASE_URL" >> "$APP_DIR/.env"
-echo "DATABASE_URL_EXTERNAL=$DATABASE_URL_EXTERNAL" >> "$APP_DIR/.env"
+# sudo docker volume rm myapp_postgres_data   
 
-# These are just for the demo of env vars
-echo "SECRET_KEY=$SECRET_KEY" >> "$APP_DIR/.env"
-echo "NEXT_PUBLIC_SAFE_KEY=$NEXT_PUBLIC_SAFE_KEY" >> "$APP_DIR/.env"
 
-# Install Nginx
-sudo apt install nginx -y
 
-# Remove old Nginx config (if it exists)
-sudo rm -f /etc/nginx/sites-available/myapp
-sudo rm -f /etc/nginx/sites-enabled/myapp
 
-# Stop Nginx temporarily to allow Certbot to run in standalone mode
-sudo systemctl stop nginx
+# nano ./myapp/.env
 
-# Obtain SSL certificate using Certbot standalone mode
-sudo apt install certbot -y
-sudo certbot certonly --standalone -d $DOMAIN_NAME --non-interactive --agree-tos -m $EMAIL
+# CREATE EXTENSION ulid;
+# ALTER SYSTEM SET shared_preload_libraries = 'ulid';
+# SHOW shared_preload_libraries;
 
-# Ensure SSL files exist or generate them
-if [ ! -f /etc/letsencrypt/options-ssl-nginx.conf ]; then
-  sudo wget https://raw.githubusercontent.com/certbot/certbot/main/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf -P /etc/letsencrypt/
-fi
+# EXPLAIN ANALYZE SELECT gen_ulid()  FROM generate_series(1,1000000); 
+# EXPLAIN ANALYZE SELECT gen_random_uuid()  FROM generate_series(1,1000000); 
 
-if [ ! -f /etc/letsencrypt/ssl-dhparams.pem ]; then
-  sudo openssl dhparam -out /etc/letsencrypt/ssl-dhparams.pem 2048
-fi
 
-# Create Nginx config with reverse proxy, SSL support, rate limiting, and streaming support
-sudo cat > /etc/nginx/sites-available/myapp <<EOL
-limit_req_zone \$binary_remote_addr zone=mylimit:10m rate=10r/s;
+# https://www.enterprisedb.com/download-postgresql-binaries
+# C:\Users\aliqa\AppData\Roaming\DBeaverData\drivers\clients\postgresql\win
 
-server {
-    listen 80;
-    server_name $DOMAIN_NAME;
 
-    # Redirect all HTTP requests to HTTPS
-    return 301 https://\$host\$request_uri;
-}
 
-server {
-    listen 443 ssl;
-    server_name $DOMAIN_NAME;
+# ####################
+# # Add pg_dump in a new stage
+# ####################
+# FROM base as pg_dump
 
-    ssl_certificate /etc/letsencrypt/live/$DOMAIN_NAME/fullchain.pem;
-    ssl_certificate_key /etc/letsencrypt/live/$DOMAIN_NAME/privkey.pem;
-    include /etc/letsencrypt/options-ssl-nginx.conf;
-    ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;
+# # Install pg_dump matching the PostgreSQL major version
+# RUN apt-get update && \
+#     apt-get install -y --no-install-recommends postgresql-client-${postgresql_major} && \
+#     rm -rf /var/lib/apt/lists/*
 
-    # Enable rate limiting
-    limit_req zone=mylimit burst=20 nodelay;
 
-    location / {
-        proxy_pass http://localhost:3000;
-        proxy_http_version 1.1;
-        proxy_set_header Upgrade \$http_upgrade;
-        proxy_set_header Connection 'upgrade';
-        proxy_set_header Host \$host;
-        proxy_cache_bypass \$http_upgrade;
+# CREATE EXTENSION ulid;
 
-        # Disable buffering for streaming support
-        proxy_buffering off;
-        proxy_set_header X-Accel-Buffering no;
-    }
-}
-EOL
+# EXPLAIN ANALYZE SELECT gen_ulid()  FROM generate_series(1,1000000); 
+# EXPLAIN ANALYZE SELECT gen_random_uuid()  FROM generate_series(1,1000000); 
 
-# Create symbolic link if it doesn't already exist
-sudo ln -s /etc/nginx/sites-available/myapp /etc/nginx/sites-enabled/myapp
 
-# Restart Nginx to apply the new configuration
-sudo systemctl restart nginx
 
-# Build and run the Docker containers from the app directory (~/myapp)
-cd $APP_DIR
-sudo docker-compose up --build -d
 
-# Check if Docker Compose started correctly
-if ! sudo docker-compose ps | grep "Up"; then
-  echo "Docker containers failed to start. Check logs with 'docker-compose logs'."
-  exit 1
-fi
 
-# Output final message
-echo "Deployment complete. Your Next.js app and PostgreSQL database are now running. 
-Next.js is available at https://$DOMAIN_NAME, and the PostgreSQL database is accessible from the web service.
 
-The .env file has been created with the following values:
-- POSTGRES_USER
-- POSTGRES_PASSWORD (randomly generated)
-- POSTGRES_DB
-- DATABASE_URL
-- DATABASE_URL_EXTERNAL
-- SECRET_KEY
-- NEXT_PUBLIC_SAFE_KEY"
+# //////////////////
+# ARG PG_MAJOR=16
+
+# FROM postgres:${PG_MAJOR}
+# RUN apt-get update && apt-get upgrade -y
+
+# ENV build_deps ca-certificates \
+#   git \
+#   build-essential \
+#   libpq-dev \
+#   postgresql-server-dev-${PG_MAJOR} \
+#   curl \
+#   libreadline6-dev \
+#   zlib1g-dev
+
+
+# RUN apt-get install -y --no-install-recommends $build_deps pkg-config cmake
+
+# WORKDIR /home/postgres
+
+# ENV HOME=/home/postgres
+# ENV PATH=/home/postgres/.cargo/bin:$PATH
+
+# RUN chown postgres:postgres /home/postgres
+
+# USER postgres
+
+# RUN \
+#   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --no-modify-path --profile minimal --default-toolchain nightly && \
+#   rustup --version && \
+#   rustc --version && \
+#   cargo --version
+
+
+# RUN cargo install cargo-pgrx --version 0.11.2 --locked
+
+# RUN cargo pgrx init --pg${PG_MAJOR} download
+
+# USER root
+
+
+# RUN mkdir -p /tmp/build \
+#   && cd /tmp/build \
+#   && git clone https://github.com/pksunkara/pgx_ulid \
+#   && cd pgx_ulid \
+#   && cargo pgrx install
+
+# RUN rm -fr /tmp/build \
+#   && apt-get clean \
+#   && apt-get autoremove -y $build_deps
